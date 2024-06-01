4f7a04e80510cde9b3d97d5cb6f24cb697155834
salahtaissir <salahtaissir61@gmail.com>
2024-06-01 14:34:21 +0200

    Create contribution.py
    contribution Hayat

---
+++ contribution.py
@@ -0,0 +1,9 @@
+# Taissir
+git add Groupe.md
+git commit -m "Ajout de la composition du groupe"
+git push origin main
+
+# Hayat (fictif) simulation 
+git add contribution.py
+git commit -m "Hayat: Ajout de la nouvelle fonctionnalité"
+git push origin main
